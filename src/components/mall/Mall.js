fa8199ceec6dc78ecc7dee5607481aa76e01f4b6
DoYourLove <xyf15620168540@163.com>
2020-08-14 17:18:05 +0800

    [xiaoyifan] feat : fix the mall

--- src/components/mall/Mall.js
+++ src/components/mall/Mall.js
@@ -1,5 +1,12 @@
 import React from 'react'
 import Product from './Product'
+import axios from 'axios';
+// import './components/mall/Mall.css'
+
+// let fetch = axios.create({
+//     baseURL: "http://localhost:8080", // 这里是本地express启动的服务地址
+//     timeout: 5000 // request timeout
+// })
 
 class Mall extends React.Component{
 
@@ -7,7 +14,19 @@ class Mall extends React.Component{
         products:[
             {
                 img: 'https://img14.360buyimg.com/n0/jfs/t4705/83/2924377281/70031/aed9bbd3/58f5629dN79b4406c.jpg',
-                name: '可乐',
+                name: '测试',
+                price: '5',
+                unit: '罐',
+            },
+            {
+                img: 'https://img14.360buyimg.com/n0/jfs/t4705/83/2924377281/70031/aed9bbd3/58f5629dN79b4406c.jpg',
+                name: '测试2',
+                price: '5',
+                unit: '罐',
+            },
+            {
+                img: 'https://img14.360buyimg.com/n0/jfs/t4705/83/2924377281/70031/aed9bbd3/58f5629dN79b4406c.jpg',
+                name: '测试3',
                 price: '5',
                 unit: '罐',
             }
@@ -15,13 +34,16 @@ class Mall extends React.Component{
     }
 
     render() {
-        return <div>
-            <Product 
-                img={this.state.products[0].img} 
-                name={this.state.products[0].name} 
-                price={this.state.products[0].price} 
-                unit={this.state.products[0].unit}
-            ></Product>
+        return <div className="mall">
+            {this.state.products.map((item) => 
+                <Product
+                    key={item.name}
+                    img={item.img} 
+                    name={item.name} 
+                    price={item.price} 
+                    unit={item.unit}
+                ></Product>
+            )}
         </div>
     }
 }
