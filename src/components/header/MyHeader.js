649d894ef78cd4b5b282044eeb4ffec93a29ebb1
DoYourLove <xyf15620168540@163.com>
2020-08-24 01:33:47 +0800

    [XiaoYiFan] feat : fix head select

--- src/components/header/MyHeader.js
+++ src/components/header/MyHeader.js
@@ -5,13 +5,37 @@ import { ShoppingCartOutlined, HomeOutlined, PlusOutlined } from '@ant-design/ic
 const {Header} = Layout;
 
 class MyHeader extends React.Component{
+
+    state = {
+        mall: '1',
+        orders: '2',
+        addproduct: '3'
+    }
+
+    componentDidMount() {
+        if(window.location.pathname === '/orders') {
+            this.setState({
+                mall: '2',
+                orders: '1',
+                addproduct: '3'
+            })
+        }
+        if(window.location.pathname === '/add-product') {
+            this.setState({
+                mall: '2',
+                orders: '3',
+                addproduct: '1'
+            })
+        }
+    }
+
     render() {
         return (
             <Header>
-                <Menu theme="dark" mode="horizontal">
-                    <Menu.Item key="1"><Link to='/mall'><HomeOutlined />商城</Link></Menu.Item>
-                    <Menu.Item key="2"><Link to='/orders'><ShoppingCartOutlined />订单</Link></Menu.Item>
-                    <Menu.Item key="3"><Link to='/add-product'><PlusOutlined />添加商品</Link></Menu.Item>
+                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+                    <Menu.Item key={[this.state.mall]}><Link to='/mall'><HomeOutlined />商城</Link></Menu.Item>
+                    <Menu.Item key={[this.state.orders]}><Link to='/orders'><ShoppingCartOutlined />订单</Link></Menu.Item>
+                    <Menu.Item key={[this.state.addproduct]}><Link to='/add-product'><PlusOutlined />添加商品</Link></Menu.Item>
                 </Menu>
             </Header>
         )
