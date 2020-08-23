a9dc16d26b5abc7ac2e5e4731591150cdaab8589
DoYourLove <xyf15620168540@163.com>
2020-08-23 10:43:41 +0800

    [XiaoYiFan] feat : add head icon

--- src/components/header/MyHeader.js
+++ src/components/header/MyHeader.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Layout, Menu} from 'antd';
 import {Link} from 'react-router-dom'
+import { ShoppingCartOutlined, HomeOutlined, PlusOutlined } from '@ant-design/icons'
 const {Header} = Layout;
 
 class MyHeader extends React.Component{
@@ -8,9 +9,9 @@ class MyHeader extends React.Component{
         return (
             <Header>
                 <Menu theme="dark" mode="horizontal">
-                    <Menu.Item key="1"><Link to='/mall'>商城</Link></Menu.Item>
-                    <Menu.Item key="2"><Link to='/orders'>订单</Link></Menu.Item>
-                    <Menu.Item key="3"><Link to='/add-product'>添加商品</Link></Menu.Item>
+                    <Menu.Item key="1"><Link to='/mall'><HomeOutlined />商城</Link></Menu.Item>
+                    <Menu.Item key="2"><Link to='/orders'><ShoppingCartOutlined />订单</Link></Menu.Item>
+                    <Menu.Item key="3"><Link to='/add-product'><PlusOutlined />添加商品</Link></Menu.Item>
                 </Menu>
             </Header>
         )
